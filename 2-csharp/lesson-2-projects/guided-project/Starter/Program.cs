e5ce18a90bed81a781bfd388c3bcd42ee87f08f8
uriapete <uriarteangelo@gmail.com>
2023-10-27 16:00:38 -0700

    2-l2-g - Program.cs - create emmaScores arr

--- 2-csharp/lesson-2-projects/guided-project/Starter/Program.cs
+++ 2-csharp/lesson-2-projects/guided-project/Starter/Program.cs
@@ -5,12 +5,7 @@ int currentAssignments = 5;
 
 int[] sophiaScores=new int[5]{90,86,87,98,100};
 int[] andrewScores=new int[5]{92,89,81,96,90};
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
+int[] emmaScores=new int[5]{90,85,87,98,68};
 
 int logan1 = 90;
 int logan2 = 95;
@@ -30,7 +25,7 @@ decimal loganScore;
 
 // sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
 // andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
+// emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
 loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
 
 sophiaScore = (decimal)sophiaSum / currentAssignments;
