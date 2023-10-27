3ed110f989c6b0bf99392b3c067b042499ce746f
uriapete <uriarteangelo@gmail.com>
2023-10-27 16:02:15 -0700

    2-l2-g - Program.cs - delete sum calculations

--- 2-csharp/lesson-2-projects/guided-project/Starter/Program.cs
+++ 2-csharp/lesson-2-projects/guided-project/Starter/Program.cs
@@ -18,11 +18,6 @@ decimal andrewScore;
 decimal emmaScore;
 decimal loganScore;
 
-// sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-// andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-// emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-// loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
-
 sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
 emmaScore = (decimal)emmaSum / currentAssignments;
